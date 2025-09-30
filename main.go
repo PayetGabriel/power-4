beae01dc1ef8ffb1696fc9bbcd000bd8ab40a921
Claxid <hohohjiji651@gmail.com>
2025-09-30 15:04:45 +0200

    Main choisis port libre

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 )
 
 func main() {
+	// On demande au système d'attribuer un port libre (":0")
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+
+	// On récupère le port attribué
+	port := listener.Addr().(*net.TCPAddr).Port
+	fmt.Printf("Serveur démarré sur le port %d...\n", port)
+
+	// On lance le serveur
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Bienvenue sur mon serveur Go !")
 	})
 
-	fmt.Println("Serveur démarré sur le port 8080...")
-	http.ListenAndServe(":8080", nil)
+	http.Serve(listener, nil)
 }
