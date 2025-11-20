f8a357462feb99c46f3f605109c9d33403f18a2f
Claxid <hohojiji651@gmail.com>
2025-11-20 16:49:24 +0100

    Delete static directory

--- static/difficulty.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".difficulty").forEach(btn => {
-  btn.addEventListener("click", () => {
-    const mode = btn.dataset.mode;
-    window.location.href = `/game?mode=${mode}`;
-  });
-});
